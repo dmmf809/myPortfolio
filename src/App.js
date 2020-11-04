f2da646f0a214c3871301382aa9a8a3c842480b6
dmmf809 <daeniellef1@gmail.com>
2020-11-04 13:02:22 -0800

    initial commit completed MyNavbar component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import MyNavbar from './components/my-navbar/MyNavbar';
+
+function App() {
+  return (
+    <div>
+        <MyNavbar />
+        My Portfolio
+    </div>
+  );
+}
+
+export default App;
---
+++ src/themes/theme.js
@@ -0,0 +1,4 @@
+export const theme = {
+    primary: "#abe5f5",
+    texColor: "#ffffff"
+};
